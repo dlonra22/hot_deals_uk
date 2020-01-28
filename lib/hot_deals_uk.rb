e23814b0f6668f6d11802a165b0bdd88b0bbd916
arnie b <arnold.bwaila@gmail.com>
2020-01-28 17:24:57 +0000

    cli.rb

--- lib/hot_deals_uk.rb
+++ lib/hot_deals_uk.rb
@@ -1,6 +1,2 @@
-require "hot_deals_uk/version"
-
-module HotDealsUk
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative "./hot_deals_uk/version"
+require_relative "./hot_deals_uk/cli"
---
+++ lib/hot_deals_uk/cli.rb
@@ -0,0 +1,5 @@
+class HotDealsUk::CLI
+  def call
+    puts "Welcome to hot deals uk. Find hottest, newest and trending deals across the uk"
+  end
+end
