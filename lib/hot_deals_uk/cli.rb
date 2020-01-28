4c01ff7a06961f9d9197015a51ad6523a8e23930
arnie b <arnold.bwaila@gmail.com>
2020-01-28 21:08:11 +0000

    stubbed out cli

--- lib/hot_deals_uk/cli.rb
+++ lib/hot_deals_uk/cli.rb
@@ -1,5 +1,19 @@
 class HotDealsUk::CLI
   def call
     puts "Welcome to hot deals uk. Find hottest, newest and trending deals across the uk"
+    deal_options 
+    #get_input
+    #display_deals
   end
+  
+  def deal_options
+    puts "Please select from the options below "
+    puts "1. Newest Deals"
+    puts "2. Hottest Deals"
+    puts "3. Trending Deals"
+    puts "4. Hot and Trending"
+    puts "5. Hot and New"
+    puts "6. Newest Trending Deals"
+  end
+    
 end
