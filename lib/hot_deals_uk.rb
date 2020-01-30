fbeb7eeb78e3ba1b74d77742a2af9b54825f7eaa
arnie b <arnold.bwaila@gmail.com>
2020-01-30 01:27:50 +0000

    hot_deals_uk.gemspec

--- lib/hot_deals_uk.rb
+++ lib/hot_deals_uk.rb
@@ -1,5 +1,5 @@
 require "nokogiri"
-require 'open-uri' #not sure if i need this but just in case
+require 'open-uri' 
 require_relative "./hot_deals_uk/version"
 require_relative "./hot_deals_uk/cli"
 require_relative "./hot_deals_uk/hotdeal"
