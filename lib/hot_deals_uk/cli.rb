58f2f554715e273e7ac6779f1517c4d80242616c
arnie b <arnold.bwaila@gmail.com>
2020-01-30 23:45:42 +0000

    cli.rb

--- lib/hot_deals_uk/cli.rb
+++ lib/hot_deals_uk/cli.rb
@@ -71,6 +71,7 @@ class HotDealsUk::CLI
       elsif input =='back'
               Gem.win_platform? ? (system "cls") : (system "clear")
               if sender.to_i > 0
+               input = nil
                sender = input
               else
               get_input
