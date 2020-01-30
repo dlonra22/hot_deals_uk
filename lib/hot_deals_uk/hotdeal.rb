0675af1c1ee82b5128aa9fb410afecd94fb06da0
arnie b <arnold.bwaila@gmail.com>
2020-01-30 22:32:36 +0000

    hotdeal.rb

--- lib/hot_deals_uk/hotdeal.rb
+++ lib/hot_deals_uk/hotdeal.rb
@@ -1,9 +1,9 @@
 class HotDealsUk::HOTDEAL
-  attr_accessor :title, :price, :hotness, :wheretofind
+  attr_accessor :title, :price, :hotness, :wheretofind, :trend_rating
   
-  @@hotdeals = []
+  @@hottest = []
   @@trending = []
-  @@newdeals = []
+  @@newest = []
   @@highlights = []
  
   def self.alldeals
@@ -13,22 +13,22 @@ class HotDealsUk::HOTDEAL
  def self.scrape_get_page
     #gets hot deals from appropriate page
     highlights = Nokogiri::HTML(open("https://www.hotukdeals.com"))
-    newdeals =  Nokogiri::HTML(open("https://www.hotukdeals.com/new"))
+    newest =  Nokogiri::HTML(open("https://www.hotukdeals.com/new"))
     hotdeals =  Nokogiri::HTML(open("https://www.hotukdeals.com/hot"))
     trending =  Nokogiri::HTML(open("https://www.hotukdeals.com/discussed"))
-    [highlights, newdeals, hotdeals, trending]
+    [highlights, newest, hotdeals, trending]
   end
   
   def self.scrape_get_deals
     #collection of deals
      highlights = self.scrape_get_page[0].search("div.threadGrid")
-     newdeals = scrape_get_page[1].search("div.threadGrid")
+     newest = scrape_get_page[1].search("div.threadGrid")
      hotdeals = self.scrape_get_page{2}.search("div.threadGrid")
      trending  = self.scrape_get_page[3].search("div.threadGrid")
-    [highlights, newdeals, hotdeals, trending]
+    [highlights, newest, hotdeals, trending]
   end
   
-  def self.scrape_make_deals
+  def self.scrape_make_highlights
     # makes hotdeal instances
     self.reset_all
     self.scrape_get_deals[0].each_with_index do |d, i|
@@ -38,13 +38,55 @@ class HotDealsUk::HOTDEAL
         deal.hotness = d.css("span.cept-vote-temp.vote-temp.vote-temp--hot").text.strip
         deal.wheretofind = d.css("span.text--b text--color-brandPrimary cept-merchant-name").text.strip
         deal.trend_rating = d.css("cept-comment-link.btn space--h-3.btn--mode-boxSec.cept-comment-link-id-3396545 svg span").text.strip
-        @@alldeals << deal
+        @@highlights << deal
     end
-    @@alldeals
+    @@highlights
+  end
+  
+  def self.scrape_make_newest
+    self.scrape_get_deals[1].each_with_index do |d, i|
+        deal = HotDealsUk::HOTDEAL.new 
+        deal.title = d.css("strong.thread-title a").text.strip
+        deal.price = d.css("span.thread-price").text.strip
+        deal.hotness = d.css("span.cept-vote-temp.vote-temp.vote-temp--hot").text.strip
+        deal.wheretofind = d.css("span.text--b text--color-brandPrimary cept-merchant-name").text.strip
+        deal.trend_rating = d.css("cept-comment-link.btn space--h-3.btn--mode-boxSec.cept-comment-link-id-3396545 svg span").text.strip
+        @@newest << deal
+    end
+    @@newest
+  end
+  
+  def self.scrape_make_hottest
+    self.scrape_get_deals[2].each_with_index do |d, i|
+        deal = HotDealsUk::HOTDEAL.new 
+        deal.title = d.css("strong.thread-title a").text.strip
+        deal.price = d.css("span.thread-price").text.strip
+        deal.hotness = d.css("span.cept-vote-temp.vote-temp.vote-temp--hot").text.strip
+        deal.wheretofind = d.css("span.text--b text--color-brandPrimary cept-merchant-name").text.strip
+        deal.trend_rating = d.css("cept-comment-link.btn space--h-3.btn--mode-boxSec.cept-comment-link-id-3396545 svg span").text.strip
+        @@hottest << deal
+    end
+    @@hottest
+  end
+    
+  def self.scrape_make_trending
+    self.scrape_get_deals[3].each_with_index do |d, i|
+        deal = HotDealsUk::HOTDEAL.new 
+        deal.title = d.css("strong.thread-title a").text.strip
+        deal.price = d.css("span.thread-price").text.strip
+        deal.hotness = d.css("span.cept-vote-temp.vote-temp.vote-temp--hot").text.strip
+        deal.wheretofind = d.css("span.text--b text--color-brandPrimary cept-merchant-name").text.strip
+        deal.trend_rating = d.css("cept-comment-link.btn space--h-3.btn--mode-boxSec.cept-comment-link-id-3396545 svg span").text.strip
+        @@trending << deal
+    end
+    @@trending
  end
   
   def self.reset_all
-   @@alldeals.clear
+   @@trending.clear
+   @@hottest.clear
+   @@newest.clear
+   @@highlights.clear
   end
 end
   
