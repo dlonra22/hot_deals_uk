bf2c9d5d4f64053ea37063956b729ff7a02609c5
arnie b <arnold.bwaila@gmail.com>
2020-01-30 04:45:26 +0000

    hotdeal.rb

--- lib/hot_deals_uk/hotdeal.rb
+++ lib/hot_deals_uk/hotdeal.rb
@@ -31,7 +31,7 @@ class HotDealsUk::HOTDEAL
     # makes hotdeal instances
     self.scrape_get_deals.each do |d|
         deal = self.new
-        deal.title = d.css("strong.thread-title").attribute("title").value
+        deal.title = d.css("strong.thread-title a").attribute("title").value
     end
     binding.pry
   end
