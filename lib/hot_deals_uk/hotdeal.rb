c1429e07ffbc134497a1d0aa1a7ee6f7da3e5f76
arnie b <arnold.bwaila@gmail.com>
2020-01-31 01:39:14 +0000

    hotdeal.rb

--- lib/hot_deals_uk/hotdeal.rb
+++ lib/hot_deals_uk/hotdeal.rb
@@ -47,29 +47,26 @@ class HotDealsUk::HOTDEAL
         deal.hotness = d.css("span.cept-vote-temp.vote-temp.vote-temp--hot").text.strip
         deal.wheretofind = d.css("span.text--b.text--color-brandPrimary.cept-merchant-name").text.strip
         deal.trend_rating = d.css("cept-comment-link.btn.space--h-3.btn--mode-boxSec.cept-comment-link-id-3396545 svg span").text.strip
-        deal.pop
+        deal
   end
   
   def self.scrape_make_highlights
     self.scrape_get_deals[0].each_with_index do |d, i|
-       deal =  self.make_deals(d)
-        @@highlights << deal
+      @@highlights << self.make_deals(d)
     end
     @@highlights
   end
   
   def self.scrape_make_newest
     self.scrape_get_deals[1].each_with_index do |d, i|
-        deal =  self.make_deals(d)
-        @@newest << deal
+        @@newest << self.make_deals(d)
     end
     @@newest
   end
   
   def self.scrape_make_hottest
     self.scrape_get_deals[2].each_with_index do |d, i|
-        deal =  self.make_deals(d)
-        @@hottest << deal
+         @@hottest << self.make_deals(d)
     end
     @@hottest
   end
